8bb5046aaa9a062edefd4d2d1529ac6a4a3f52ee
DeepuKr0315 <125431399+DeepuKr0315@users.noreply.github.com>
2025-04-20 23:20:08 +0530

    adding new file

--- src/Array/Reverse.java
+++ src/Array/Reverse.java
@@ -1,4 +1,32 @@
 package Array;
-
+import java.util.*;
 public class Reverse {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter the elements: ");
+        for(int i=0; i < size; i++){
+            arr[i] = sc.nextInt();
+        }
+        reverse(arr);
+        System.out.println("Reversed Array: " + Arrays.toString(arr));
+    }
+
+    static void reverse(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+        while(start <= end){
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    static void swap(int[] arr, int start, int end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+    }
 }
