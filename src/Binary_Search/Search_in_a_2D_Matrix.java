d97e25e10421bed37cda6f960d03d33da52b6f4c
DeepuKr0315 <125431399+DeepuKr0315@users.noreply.github.com>
2025-04-23 10:55:08 +0530

    updating file

--- src/Binary_Search/Search_in_a_2D_Matrix.java
+++ src/Binary_Search/Search_in_a_2D_Matrix.java
@@ -3,15 +3,15 @@
 // 74. Search a 2D Matrix
 
 package Binary_Search;
-
+import java.util.*;
 public class Search_in_a_2D_Matrix {
     public static void main(String[] args) {
         int[][] matrix = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
         int target = 3;
         System.out.println();
-        System.out.println(searchMatrix(matrix, target));
+        System.out.println(Arrays.toString(searchMatrix(matrix, target)));
     }
-    public static boolean searchMatrix(int[][] matrix, int target) {
+    public static int[] searchMatrix(int[][] matrix, int target) {
         int m = matrix.length;
         int n = matrix[0].length;
         int start = 0;
@@ -20,13 +20,13 @@ public class Search_in_a_2D_Matrix {
             int mid = start + (end - start) / 2;
             int midValue = matrix[mid / 2][mid % n];
             if(target == midValue){
-                return true;
+                return new int[]{mid/2, mid % n};
             }else if(target < midValue){
                 end = mid - 1;
             }else{
                 start = mid + 1;
             }
         }
-        return false;
+        return new int[]{-1, -1};
     }
 }
